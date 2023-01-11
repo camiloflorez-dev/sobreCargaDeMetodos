38d7e905f2bd351adbb2394f706efcb9f390c69d
camiloflorez-dev <brayancamilo.florez@outlook.com>
2023-01-10 20:47:29 -0500

    carga de proyecto manejo de sobre carga de metodos

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,14 @@
+package operaciones;
+
+public class Operaciones {
+    
+    public static int sumar(int a, int b){
+        System.out.println("resultado = a + b entero");
+        return a + b;
+    }
+    
+    public static double sumar(double a, double b){
+        System.out.println("resultado 2 = a + b double");
+        return a + b;
+    }
+}
